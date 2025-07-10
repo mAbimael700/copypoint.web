ce19a165719e1397bfee9235c43ae3ef397f447d
mAbimael <mabimael700@gmail.com>
2025-07-09 21:50:47 -0600

    mercado-pago-add-payment-btn.tsx modified

--- src/features/mercadopago-payment/components/input/mercado-pago-add-payment-btn.tsx
+++ src/features/mercadopago-payment/components/input/mercado-pago-add-payment-btn.tsx
@@ -1,14 +1,24 @@
-import { MpHandshakePlumaHorizontalIcon } from '@/features/mercadopago-payment/components/icons/icons.tsx'
+import { cn } from '@/lib/utils'
+import {
+  MpHandshakeColorHorizontalIcon,
+  MpHandshakePlumaHorizontalIcon,
+} from '@/features/mercadopago-payment/components/icons/icons.tsx'
 
-const MercadoPagoAddPaymentBtn = () => {
+interface Props {
+  className?: string
+}
+
+const MercadoPagoAddPaymentBtn = ({ className }: Props) => {
   return (
     <div
-      className={
-        'bg-primary text-secondary border-primary flex h-20 cursor-pointer items-center justify-center rounded-2xl shadow transition-colors hover:bg-[#ffe700]'
-      }
+      className={cn(
+        'group bg-primary text-secondary border-primary flex h-20 cursor-pointer items-center justify-center rounded-2xl shadow transition-colors hover:bg-[#ffe700]',
+        className
+      )}
     >
       Add payment
-      <MpHandshakePlumaHorizontalIcon className={'text-secondary h-36 w-36'} />
+      <MpHandshakePlumaHorizontalIcon className='text-secondary h-36 w-36 group-hover:hidden' />
+      <MpHandshakeColorHorizontalIcon className='text-secondary hidden h-36 w-36 group-hover:block' />
     </div>
   )
 }
--- src/features/payments/components/payment-form.tsx
+++ src/features/payments/components/payment-form.tsx
@@ -1,9 +1,10 @@
+import MercadoPagoAddPaymentBtn from '@/features/mercadopago-payment/components/input/mercado-pago-add-payment-btn'
 
 
 const PaymentForm = () => {
   return (
     <div>
-
+      <MercadoPagoAddPaymentBtn className={'w-1/4'}/>
     </div>
   )
 }
