18c045a0ce19e32c50e4955650cd4b40e2a4a968
mAbimael <mabimael700@gmail.com>
2025-07-09 20:43:12 -0600

    mercadopago svg icon components added

---
+++ src/features/mercadopago-payment/components/icons/icons.tsx
@@ -0,0 +1,11 @@
+import MpHandshakeColorHorizontalIcon from '@/features/mercadopago-payment/assets/svg/MP_RGB_HANDSHAKE_color_horizontal.svg'
+import MpHandshakeColorVerticalIcon from '@/features/mercadopago-payment/assets/svg/MP_RGB_HANDSHAKE_color_vertical.svg'
+import MpHandshakePlumaHorizontalIcon from '@/features/mercadopago-payment/assets/svg/MP_RGB_HANDSHAKE_pluma_horizontal.svg'
+import MpHandshakePlumaVerticalIcon from '@/features/mercadopago-payment/assets/svg/MP_RGB_HANDSHAKE_pluma_vertical.svg'
+
+export {
+  MpHandshakePlumaVerticalIcon,
+  MpHandshakePlumaHorizontalIcon,
+  MpHandshakeColorVerticalIcon,
+  MpHandshakeColorHorizontalIcon,
+}
