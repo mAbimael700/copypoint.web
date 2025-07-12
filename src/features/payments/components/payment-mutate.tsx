2e53d10721dce52aa65592f54bb3d94418c35b7d
mAbimael <mabimael700@gmail.com>
2025-07-11 22:39:43 -0600

    payment-mutate.tsx modified

--- src/features/payments/components/payment-mutate.tsx
+++ src/features/payments/components/payment-mutate.tsx
@@ -1,13 +1,18 @@
 import { Main } from '@/components/layout/main'
 import MercadoPagoAddPaymentBtn from '@/features/mercadopago-payment/components/input/mercado-pago-add-payment-btn'
+import SaleProfileSimpleList from '@/features/saleprofile/components/sale-profile-simple-list'
+import useSaleProfiles from '@/features/saleprofile/hooks/useSaleProfiles'
 
 
 const PaymentMutate = () => {
+  const {saleProfiles} = useSaleProfiles()
   return (
     <Main>
       <div>
         <MercadoPagoAddPaymentBtn className={'w-1/4'} />
       </div>
+
+      <SaleProfileSimpleList saleProfiles={saleProfiles} />
     </Main>
   )
 }
